92b57df1de1f10cb2b75f92a7f8fe62d80a1cd90
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-10-08 12:48:15 +0900

    add functions, permutation and combination

--- lib/combinatorics.ex
+++ lib/combinatorics.ex
@@ -4,15 +4,46 @@ defmodule Combinatorics do
   """
 
   @doc """
-  Hello world.
+  Generate Permutaitons.
 
-  ## Examples
-
-      iex> Combinatorics.hello()
-      :world
-
+      iex> Combinatorics.permutation([1, 2, 3])
+      [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
+      iex> Combinatorics.permutation([1, 2, 3], 2)
+      [[1, 2], [1, 3], [2, 1], [2, 3], [3, 1], [3, 2]]
+      iex> Combinatorics.permutation([1, 2, 3], 0)
+      []
   """
-  def hello do
-    :world
+  def permutation(list), do: permutation(list, Enum.count(list))
+  def permutation(_, n) when n <= 0, do: []
+  def permutation(list, 1), do: Enum.map(list, &[&1])
+  def permutation(list, n) do
+    list
+    |> Enum.flat_map(fn elem ->
+      list
+      |> List.delete(elem)
+      |> permutation(n - 1)
+      |> Enum.map(&[elem | &1])
+    end)
+  end
+
+  @doc """
+  Generate Combinations.
+
+      iex> Combinatorics.combination([1, 2, 3], 2)
+      [[1, 2], [1, 3], [2, 3]]
+      iex> Combinatorics.combination([1, 2, 3], 3)
+      [[1, 2, 3]]
+      iex> Combinatorics.combination([1, 2, 3], 0)
+      []
+  """
+  def combination([], _), do: []
+  def combination(_, n) when n <= 0, do: []
+  def combination(list, 1), do: Enum.map(list, &[&1])
+  def combination([first | rest], n) do
+    prev = combination(rest, n)
+    rest
+    |> combination(n - 1)
+    |> Enum.reverse()
+    |> Enum.reduce(prev, &[[first | &1] | &2])
   end
 end
--- test/combinatorics_test.exs
+++ test/combinatorics_test.exs
@@ -2,7 +2,112 @@ defmodule CombinatoricsTest do
   use ExUnit.Case
   doctest Combinatorics
 
-  test "greets the world" do
-    assert Combinatorics.hello() == :world
+  import Combinatorics
+
+  describe "permute all elements" do
+    test "empty list" do
+      assert permutation([]) == []
+    end
+
+    test "has an element" do
+      assert permutation([1]) == [[1]]
+    end
+
+    test "has two elements" do
+      assert permutation([1, 2]) == [
+        [1, 2],
+        [2, 1]
+      ]
+    end
+
+    test "has three elements" do
+      assert permutation([1, 2, 3]) == [
+        [1, 2, 3], [1, 3, 2],
+        [2, 1, 3], [2, 3, 1],
+        [3, 1, 2], [3, 2, 1]
+      ]
+    end
+
+    test "has four elements" do
+      assert permutation([1, 2, 3, 4]) == [
+        [1, 2, 3, 4], [1, 2, 4, 3], [1, 3, 2, 4], [1, 3, 4, 2], [1, 4, 2, 3], [1, 4, 3, 2],
+        [2, 1, 3, 4], [2, 1, 4, 3], [2, 3, 1, 4], [2, 3, 4, 1], [2, 4, 1, 3], [2, 4, 3, 1],
+        [3, 1, 2, 4], [3, 1, 4, 2], [3, 2, 1, 4], [3, 2, 4, 1], [3, 4, 1, 2], [3, 4, 2, 1],
+        [4, 1, 2, 3], [4, 1, 3, 2], [4, 2, 1, 3], [4, 2, 3, 1], [4, 3, 1, 2], [4, 3, 2, 1]
+      ]
+    end
+  end
+
+  describe "permute some elements" do
+    test "P(3, 1)" do
+      assert permutation([1, 2, 3], 1) == [[1], [2], [3]]
+    end
+
+    test "P(3, 2)" do
+      assert permutation([1, 2, 3], 2) == [
+        [1, 2], [1, 3],
+        [2, 1], [2, 3],
+        [3, 1], [3, 2]
+      ]
+    end
+
+    test "P(4, 3)" do
+      assert permutation([1, 2, 3, 4], 3) == [
+        [1, 2, 3], [1, 2, 4], [1, 3, 2], [1, 3, 4], [1, 4, 2], [1, 4, 3],
+        [2, 1, 3], [2, 1, 4], [2, 3, 1], [2, 3, 4], [2, 4, 1], [2, 4, 3],
+        [3, 1, 2], [3, 1, 4], [3, 2, 1], [3, 2, 4], [3, 4, 1], [3, 4, 2],
+        [4, 1, 2], [4, 1, 3], [4, 2, 1], [4, 2, 3], [4, 3, 1], [4, 3, 2]
+      ]
+    end
+
+    test "P(4, 0)" do
+      assert permutation([1, 2, 3, 4], 0) == []
+    end
+
+    test "P(4, 5)" do
+      assert permutation([1, 2, 3, 4], 5) == []
+    end
+  end
+
+  describe "combine" do
+    test "C(3, 1)" do
+      assert combination([1,2,3], 1) == [[1], [2], [3]]
+    end
+
+    test "C(3, 2)" do
+      assert combination([1,2,3], 2) == [[1, 2], [1, 3], [2 ,3]]
+    end
+
+    test "C(3, 3)" do
+      assert combination([1,2,3], 3) == [[1, 2, 3]]
+    end
+
+    test "C(4, -1)" do
+      assert combination([1, 2, 3, 4], -1) == []
+    end
+
+    test "C(4, 0)" do
+      assert combination([1, 2, 3, 4], 0) == []
+    end
+
+    test "C(4, 1)" do
+      assert combination([1, 2, 3, 4], 1) == [[1], [2], [3], [4]]
+    end
+
+    test "C(4, 2)" do
+      assert combination([1, 2, 3, 4], 2) == [[1, 2], [1, 3], [1, 4], [2, 3], [2, 4], [3, 4]]
+    end
+
+    test "C(4, 3)" do
+      assert combination([1, 2, 3, 4], 3) == [[1, 2, 3], [1, 2, 4], [1, 3, 4], [2, 3, 4]]
+    end
+
+    test "C(4, 4)" do
+      assert combination([1, 2, 3, 4], 4) == [[1, 2, 3, 4]]
+    end
+
+    test "C(4, 5)" do
+      assert combination([1, 2, 3, 4], 5) == []
+    end
   end
 end
